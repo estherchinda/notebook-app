b1ec2c02090ccccdd82334ba56a02352be499868
Quietonset <davidakachi9@gmail.com>
2025-02-20 19:25:32 +0100

    testing deployment #2

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/notebook-app/',
   plugins: [
     vue(),
     vueDevTools(),
@@ -25,5 +26,4 @@ export default defineConfig({
       }
     }
   },
-  base: '/notebook-app/',
 })
